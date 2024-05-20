dbdd164a720cd5fac1eece358bc1a40effca207a
meherab303 <nahinrahman87@gmail.com>
2024-05-21 00:27:17 +0600

    student data inserted successfully

---
+++ src/modules/student/student.interface.ts
@@ -0,0 +1,36 @@
+export type Guardian = {
+  fatherName: string;
+  fatherOccupation: string;
+  fatherContactNo: string;
+  motherName: string;
+  motherOccupation: string;
+  motherContactNo: string;
+};
+export type Name = {
+  firstName: string;
+  middleName: string;
+  lastName: string;
+};
+export type LocalGuardian = {
+  name: string;
+  occupation: string;
+  contactNo: string;
+  address: string;
+};
+
+export type Student = {
+  id: string;
+  name: Name;
+  gender: "male" | "female";
+  dateOfBirth?: string;
+  email: string;
+  emergencyContactNo: string;
+  bloodGroup?: "A+" | "A-" | "B+" | "B-" | "AB+" | "AB-" | "O+" | "O-";
+
+  presentAddress: string;
+  permanentAddress: string;
+  guardian: Guardian;
+  localGuardian: LocalGuardian;
+  profileImg?: string;
+  isActive: "active" | "blocked";
+};
---
+++ src/modules/student/student.service.ts
@@ -0,0 +1,20 @@
+import { StudentModel } from "../student.model";
+import { Student } from "./student.interface";
+
+const createStudentIntoDb = async (student: Student) => {
+  const result = await StudentModel.create(student);
+  return result;
+};
+const getAllStudentFromDb = async () => {
+  const result = await StudentModel.find();
+  return result;
+};
+const getSingleStudentFromDb = async (id: string) => {
+  const result = await StudentModel.findOne({ id });
+  return result;
+};
+export const StudentService = {
+  createStudentIntoDb,
+  getAllStudentFromDb,
+  getSingleStudentFromDb,
+};
